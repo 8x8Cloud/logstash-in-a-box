030b3ab6fc1ea812b1e615a62b4ebb3441eb761e
Greg Feigenson <greg.feigenson@8x8.com>
2017-01-26 15:38:32 -0800

    Spruce up docs:  * Make docs easier to follow if you're new to ElasticSearch/ELK  * Add image showing how to know what your indexer is doing  * Add image showing how to select a different time range  * Add a link to a default dashboard of the last 30 days that people can just click on.

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -24,6 +24,11 @@ if [ "$1" = 'logstash' ]; then
 	echo "loading templates..."
 	cd /usr/share/logstash/bin/kibana-goodies && ./load.sh
 
+	# TODO: this is a very hard-coded solution, but it's what we've got for now:
+	# https://discuss.elastic.co/t/kibana-5-0-ingest-api/65043/4. Check back later
+	# to see if this improves.
+	curl -XPUT http://elasticsearch-master:9200/.kibana/config/5.1.2 -d '{"defaultIndex" : "logstash-*", "buildNum": 14588}'
+
 	echo "Logging that stash!"
 	set -- su-exec logstash "$@"
 fi
